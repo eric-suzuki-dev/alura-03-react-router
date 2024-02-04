3d2b1f9cd47cac83d521a60e4119ebd32510f235
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-02-03 23:40:53 -0300

    Modulo 02

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./pages/inicio";
 import SobreMim from "./pages/SobreMim";
+import Menu from "./components/Menu";
 
 function App() {
-return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Inicio />}/>
-    <Route path="/sobremim" element={<SobreMim />}/>
-    <Route path="*" element={<div>Página não encontrada</div>}/>
-  </Routes>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
