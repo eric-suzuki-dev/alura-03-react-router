e463d972044ce11aa8149137964ff77755360768
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-02-04 23:14:10 -0300

    Modulo 03

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu";
 import Inicio from "./pages/inicio";
 import SobreMim from "./pages/SobreMim";
+import Rodape from "components/Rodape";
+import PaginaPadrao from "components/PaginaPadrao";
 
 function AppRoutes() {
   return (
@@ -9,10 +11,27 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
+
+        {/* Na rota "/", a estrutura a ser renderiza é:
+
+        <PaginaPadrao>
+          <Inicio />
+        </PaginaPadrao>
+
+        Na rota "/sobremim", a estrutura a ser renderiza é:
+
+        <PaginaPadrao>
+          <SobreMim />
+        </PaginaPadrao> */}
+
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
