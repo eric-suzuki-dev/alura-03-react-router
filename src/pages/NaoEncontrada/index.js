775e6a60feec08d32036ef8921f0280faebbdd7c
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-02-09 02:23:12 -0300

    Modulo 05

--- src/pages/NaoEncontrada/index.js
+++ src/pages/NaoEncontrada/index.js
@@ -1,7 +1,11 @@
-import styles from './NaoEncontrada.module.css'
+import BotaoPrincipal from 'components/BotaoPrincipal';
+import styles from './NaoEncontrada.module.css';
 import erro404 from 'assets/erro_404.png'
+import { useNavigate } from 'react-router-dom';
 
 export default function NaoEncontrada() {
+    const navegar = useNavigate();
+
     return (
         <>
             <div className={styles.conteudoContainer}>
@@ -10,21 +14,31 @@ export default function NaoEncontrada() {
                 <h1 className={styles.titulo}>
                     Ops! Página não encontrada.
                 </h1>
+
                 <p className={styles.paragrafo}>
                     Tem certeza de que era isso que você estava procurando?
                 </p>
+
                 <p className={styles.paragrafo}>
                     Aguarde uns instantes e recarregue a página, ou volte para a página inicial.
                 </p>
 
-                <div className={styles.botaoContainer}>
-                    <button>Voltar</button>
+                <div className={styles.botaoContainer}
+                onClick={() => navegar(-1)}
+                >
+                    <BotaoPrincipal tamanho="lg">
+                        Voltar
+                        </BotaoPrincipal>
                 </div>
 
-                <img className={styles.imagemCachorro} src={erro404} alt="Cachorro de oculos  e vestido como humana" />
+                <img
+                    className={styles.imagemCachorro}
+                    src={erro404}
+                    alt="Cachorro de óculos e vestido como humano"
+                />
             </div>
-
             <div className={styles.espacoEmBranco}></div>
         </>
+
     )
 }
